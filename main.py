886a2cc53ffce212dc863be3e86d07be0545c7db
Tamami <85936708+ktamami@users.noreply.github.com>
2022-04-01 09:03:45 +0900

    引数とかリターンいらなかった。データフォーマットとエクスポートの関数を分けた

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import datetime
 import pandas as pd
 
 today = datetime.datetime.today()
-file_date =　today.strftime("%Y%m%d")
+file_date = date = today.strftime("%Y%m%d")
 
 keywords = ["コーヒー", "珈琲", "coffee"]
-nikkei_url = "https://www.nikkei.com/search?keyword="
+nikkei_url = "https://www.nikkei.com/"
 
 class NikkeiSearch:
 
@@ -15,33 +15,37 @@ class NikkeiSearch:
         self.articles = []
         self.title_list = []
         self.desc_list = []
+        self.url_list = []
 
     def search(self, name):
-        response = requests.get(nikkei_url + f"{name}&volume=20")
+        response = requests.get(f"{nikkei_url}search?keyword={name}&volume=20")
         response_html = response.text
         soup = BeautifulSoup(response_html, "html.parser")
         titles = soup.find_all(name="h3", class_="nui-card__title")
         descriptions = soup.find_all(name="a", class_="nui-card__excerpt")
-        title_list = [title.text.replace("　", "").replace(" ", "").replace("\n", "") for title in titles]
-        url_list = [desc.get_attribute_list("href") for desc in descriptions]
-        desc_list = [desc.text.replace("　", "").replace(" ", "").replace("\n", "") for desc in descriptions]
-        return title_list, url_list, desc_list
+        self.title_list = [title.text.replace("　", "").replace(" ", "").replace("\n", "") for title in titles]
+        self.url_list = [desc.get_attribute_list("href")[0] for desc in descriptions]
+        self.desc_list = [desc.text.replace("　", "").replace(" ", "").replace("\n", "") for desc in descriptions]
 
-    def export_data(self, title, url, desc):
-        for n in range(len(title)):
+    def format_data(self):
+        for n in range(len(self.title_list)):
             article = {}
-            article["title"] = title[n]
-            article["description"] = desc[n]
-            article["url"] = url[n]
+            article["title"] = self.title_list[n]
+            article["description"] = self.desc_list[n]
+            article["url"] = self.url_list[n]
             self.articles.append(article)
+
+    def export_data(self):
         df = pd.DataFrame(self.articles)
         df.duplicated(subset=["title"])
         df.to_excel(f"{file_date}_nikkei_coffee.xlsx", encoding='utf_8_sig', index=False)
 
+
 nikkei = NikkeiSearch()
 
 for keyword in keywords:
-    title_list, url_list, desc_list = nikkei.search(keyword)
-    nikkei.export_data(title_list, url_list, desc_list)
+    nikkei.search(keyword)
+    nikkei.format_data()
+nikkei.export_data()
 
 
